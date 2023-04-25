018cc7ad8a8744927a2f1e376f01e96b2feae5d6
[InquisidorDeLapinya Sergio] <msegio2003@gmail.com>
2023-04-25 17:52:19 +0200

    cambioPag

--- ThePinecone/src/app/components/header/header.component.ts
+++ ThePinecone/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+ /* class="nav-link */
+ public navigation: boolean = false;
 
+ public onClick(): void {
+   this.navigation = !this.navigation;
+ }
 }
