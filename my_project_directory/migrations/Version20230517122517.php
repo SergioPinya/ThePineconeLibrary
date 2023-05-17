06fc79721a137ee54d34bdcbaacbaeefba22d224
[InquisidorDeLapinya Sergio] <msegio2003@gmail.com>
2023-05-17 13:25:07 +0000

    v3

--- my_project_directory/migrations/Version20230517122517.php
+++ my_project_directory/migrations/Version20230517122517.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230512233018 extends AbstractMigration
+final class Version20230517122517 extends AbstractMigration
 {
     public function getDescription(): string
     {
